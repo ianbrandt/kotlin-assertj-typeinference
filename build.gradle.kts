9dfa21f1e67867f3f0658ce42e200b8294e143c8
Ian Brandt <ian@ianbrandt.com>
2022-01-13 10:27:16 -0800

    Upgraded JUnit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 
-	testImplementation(platform("org.junit:junit-bom:5.8.1"))
+	testImplementation(platform("org.junit:junit-bom:5.8.2"))
 
 	// Compiles
 	// testImplementation("org.assertj:assertj-core:3.20.2")
